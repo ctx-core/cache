5cd242b5284394fb7d7e85e68025a40df97d45b5
Brian Takita <brian.takita@gmail.com>
2020-11-24 17:11:06 -0700

    interface instead of type

--- cache_store_type.d.ts
+++ cache_store_type.d.ts
@@ -1,6 +1,6 @@
 import type { Writable } from '@ctx-core/store';
-export declare type $cache_store_type<I extends object> = {
+export interface $cache_store_type<I extends object> {
     data: I;
     promise_a1: Promise<I>[];
-};
+}
 export declare type cache_store_type<I extends object> = Writable<$cache_store_type<I>>;
--- cache_store_type.ts
+++ cache_store_type.ts
@@ -1,6 +1,6 @@
 import type { Writable } from '@ctx-core/store'
-export type $cache_store_type<I extends object> = {
-	data: I
-	promise_a1: Promise<I>[]
+export interface $cache_store_type<I extends object> {
+	data:I
+	promise_a1:Promise<I>[]
 }
 export type cache_store_type<I extends object> = Writable<$cache_store_type<I>>
