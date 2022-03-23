8c9c4685419b80936ba981f1624cc8f762995920
Brian Takita <brian.takita@gmail.com>
2022-03-22 22:49:10 -0500

    cache_ctx$_
    	cache_ctx$__opts: + id_: factory function takes query_data & returns the cache id
    	query,be,ensure,ensure_val,load: (query_data:query_data_T, opts?:cache_ctx$__be_opts_T)

--- src/cache_ctx$_.d.ts
+++ src/cache_ctx$_.d.ts
@@ -1,18 +1,19 @@
 import type { nullish, Timeout } from '@ctx-core/function'
 import { ReadableAtom$, WritableAtom$ } from '@ctx-core/nanostores'
-export declare function cache_ctx$_<Val extends unknown = unknown, opts_data_T extends unknown = unknown>(
-	query:cache_ctx$__query_T<Val, opts_data_T>,
-	cache_ctx$__opts?:cache_ctx$__opts_T
-):cache_ctx$_T<Val, opts_data_T>;
-export interface cache_ctx$__opts_T {
+export declare function cache_ctx$_<Val extends unknown = unknown, query_data_T extends unknown = unknown>(
+	query:cache_ctx$__query_T<Val, query_data_T>,
+	cache_ctx$__opts?:cache_ctx$__opts_T<query_data_T>
+):cache_ctx$_T<Val, query_data_T>;
+export interface cache_ctx$__opts_T<query_data_T extends unknown = unknown> {
+	id_?:(query_data:query_data_T)=>string;
 	ttl?:number;
 	period?:number;
 	poll?:boolean;
 }
-export declare type cache_ctx$__query_T<Val extends unknown = unknown, opts_data_T extends unknown = unknown> =
-	(opts_data:opts_data_T, id?:string)=>Promise<Val>;
-export interface cache_ctx$__be_opts_T<opts_data_T extends unknown = unknown> extends cache_ctx$__opts_T {
-	data?:opts_data_T;
+export declare type cache_ctx$__query_T<Val extends unknown = unknown, query_data_T extends unknown = unknown> =
+	(query_data:query_data_T, opts?:cache_ctx$__opts_T)=>Promise<Val>;
+export interface cache_ctx$__be_opts_T extends cache_ctx$__opts_T {
+	id?:string;
 	ttl?:number;
 	period?:number;
 	poll?:boolean;
@@ -23,13 +24,13 @@ export interface cache_ctx_value$_T<Val extends unknown = unknown> extends Writa
 	error?:any;
 	ttl?:number;
 	period?:number;
-	poll?:number|Timeout|nullish;
+	poll?:number|Timeout|NodeJS.Timeout|nullish;
 	expiration?:Date;
 }
 export declare type cache_ctx_T<Val extends unknown = unknown> = Record<string, cache_ctx_value$_T<Val>>;
-export interface cache_ctx$_T<Val extends unknown = unknown, opts_data_T extends unknown = unknown> extends ReadableAtom$<cache_ctx_T<Val>> {
-	be:(opts:cache_ctx$__be_opts_T<opts_data_T>, id?:string)=>cache_ctx_value$_T<Val>;
-	ensure:(opts:cache_ctx$__be_opts_T<opts_data_T>, id?:string)=>Promise<cache_ctx_value$_T<Val>|nullish>;
-	ensure_val:(opts:cache_ctx$__be_opts_T<opts_data_T>, id?:string)=>Promise<Val|nullish>;
+export interface cache_ctx$_T<Val extends unknown = unknown, query_data_T extends unknown = unknown> extends ReadableAtom$<cache_ctx_T<Val>> {
+	be:(query_data:query_data_T, opts?:cache_ctx$__be_opts_T)=>cache_ctx_value$_T<Val>;
+	ensure:(query_data:query_data_T, opts?:cache_ctx$__be_opts_T)=>Promise<cache_ctx_value$_T<Val>|nullish>;
+	ensure_val:(query_data:query_data_T, opts?:cache_ctx$__be_opts_T)=>Promise<Val|nullish>;
 }
 export { cache_ctx$_ as _cache_ctx, }
--- src/cache_ctx$_.js
+++ src/cache_ctx$_.js
@@ -5,19 +5,20 @@ import { atom$, split_atom$ } from '@ctx-core/nanostores'
 /** @typedef {import('./cache_ctx$_.d.ts').cache_ctx$_T}cache_ctx$_T */
 /** @typedef {import('@ctx-core/nanostores').split_atom$_ret_T}split_atom$_ret_T */
 /** @typedef {import('@ctx-core/nanostores').WritableAtom$}WritableAtom$ */
-/** @type {import('@ctx-core/object').nullish}nullish */
+/** @typedef {import('@ctx-core/object').nullish}nullish */
 /**
- * @param {cache_ctx$__query_T<unknown, cache_ctx$__be_opts_T<unknown>>} query
+ * @param {cache_ctx$__query_T<unknown, cache_ctx$__be_opts_T>} query
  * @param {cache_ctx$__opts_T}cache_ctx$__opts
  * @returns {cache_ctx$_T<unknown, unknown>}
  * @private
  */
-export function cache_ctx$_(query, cache_ctx$__opts) {
-	/** @type {split_atom$_ret_T<unknown>} */
+export function cache_ctx$_(query, cache_ctx$__opts = {}) {
+	/** @type {split_atom$_ret_T} */
 	const cache_ctx$$$ = split_atom$({})
 	/** @type {cache_ctx$_T<unknown, unknown>} */
 	const cache_ctx$ = cache_ctx$$$[0]
 	const set = cache_ctx$$$[1]
+	const id_ = cache_ctx$__opts.id_ || (query_data=>query_data.toString())
 	assign(cache_ctx$, ({
 		be,
 		ensure,
@@ -25,42 +26,44 @@ export function cache_ctx$_(query, cache_ctx$__opts) {
 	}))
 	return cache_ctx$
 	/**
-	 * @param {cache_ctx$__be_opts_T<unknown>} opts
-	 * @param {string} id
-	 * @returns {cache_ctx_value$_T<unknown>}
+	 * @param {unknown} query_data
+	 * @param {cache_ctx$__be_opts_T} opts
+	 * @returns {cache_ctx_value$_T}
 	 * @private
 	 */
-	function be(opts, id = opts.data.toString()) {
-		const cache_ctx_value$ = base_be(id)
-		load(id, opts).then()
+	function be(query_data, opts = {}) {
+		const cache_ctx_value$ = base_be(opts.id || id_(query_data))
+		load(query_data, opts).then()
 		return cache_ctx_value$
 	}
 	/**
-	 * @param {cache_ctx$__be_opts_T<unknown>} opts
-	 * @param {string} id
-	 * @returns {Promise<cache_ctx_value$_T<unknown>|nullish>}
+	 * @param {unknown} query_data
+	 * @param {cache_ctx$__be_opts_T} opts
+	 * @returns {Promise<cache_ctx_value$_T|nullish>}
 	 * @private
 	 */
-	async function ensure(opts, id = opts.data.toString()) {
-		const cache_ctx_value$ = base_be(id)
-		await load(id, opts)
+	async function ensure(query_data, opts = {}) {
+		const cache_ctx_value$ = base_be(query_data, opts)
+		await load(query_data, opts)
 		return cache_ctx_value$
 	}
 	/**
-	 * @param {cache_ctx$__be_opts_T<unknown>} opts
-	 * @param {string} id
+	 * @param {unknown} query_data
+	 * @param {cache_ctx$__be_opts_T} opts
 	 * @returns {Promise<unknown|nullish>}
 	 * @private
 	 */
-	async function ensure_val(opts, id = opts.data.toString()) {
-		return (await ensure(id, opts)).$
+	async function ensure_val(query_data, opts = {}) {
+		return (await ensure(query_data, opts)).$
 	}
 	/**
-	 * @param {string} id
-	 * @returns {cache_ctx_value$_T<unknown>}
+	 * @param {unknown} query_data
+	 * @param {cache_ctx$__be_opts_T} opts
+	 * @returns {cache_ctx_value$_T}
 	 */
-	function base_be(id) {
+	function base_be(query_data, opts = {}) {
 		const cache_ctx = cache_ctx$.$
+		const id = opts.id || id_(query_data)
 		if (!cache_ctx[id]) {
 			cache_ctx[id] = cache_ctx_val_()
 			set(cache_ctx)
@@ -68,19 +71,20 @@ export function cache_ctx$_(query, cache_ctx$__opts) {
 		return cache_ctx[id]
 	}
 	/**
-	 * @param {cache_ctx$__be_opts_T<unknown>} opts
-	 * @param {string} id
+	 * @param {unknown} query_data
+	 * @param {cache_ctx$__be_opts_T} opts
 	 * @returns {Promise<void>}
 	 */
-	async function load(opts, id = opts.data.toString()) {
+	async function load(query_data, opts = {}) {
 		const cache_ctx = cache_ctx$.$
 		const now = new Date()
+		const id = opts.id ?? id_(query_data)
 		const cache_ctx_value$ = cache_ctx[id]
 		const { expiration } = cache_ctx_value$
 		let cache_ctx_value = cache_ctx_value$.$
 		if (cache_ctx_value == null || expiration && expiration < now || opts.force) {
 			if (!cache_ctx_value$.promise) {
-				cache_ctx_value$.promise = query(id, opts.data)
+				cache_ctx_value$.promise = query(query_data)
 			}
 			try {
 				cache_ctx_value = await cache_ctx_value$.promise
@@ -112,7 +116,7 @@ export function cache_ctx$_(query, cache_ctx$__opts) {
 		}
 	}
 	/**
-	 * @returns {cache_ctx_value$_T<unknown>}
+	 * @returns {cache_ctx_value$_T}
 	 * @private
 	 */
 	function cache_ctx_val_() {
